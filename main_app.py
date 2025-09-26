563cf03c39a6a853eeb52285baec508dbf7294d5
Lusiana2312 <lusiana.torres@siemens-energy.com>
2025-09-26 11:26:32 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -4,22 +4,7 @@ import streamlit as st
 equipo = st.selectbox("Selecciona el tipo de equipo", ["CTG DPS", "Otro equipo"])
 
 # 📦 Importar función según el equipo seleccionado
-if equipo == "CTG DPS":
+if equipo == "Descargador de sobretensiones":
     from generador_ctg import exportar_excel, obtener_nombre_archivo
 elif equipo == "Otro equipo":
     from generador_otro_equipo import exportar_excel, obtener_nombre_archivo
-
-# 🎨 Parámetros de estilo
-fuente = "Calibri"
-tamaño = 9
-
-# 📥 Botón para generar y descargar
-if st.button("📊 Generar archivo Excel"):
-    archivo_excel = exportar_excel(fuente=fuente, tamaño=tamaño)
-    nombre_archivo = obtener_nombre_archivo()
-    st.download_button(
-        label="📥 Descargar archivo Excel",
-        data=archivo_excel,
-        file_name=nombre_archivo,
-        mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-    )

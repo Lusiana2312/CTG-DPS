20ce5d20463cae11bbe98c32c878d3e8b8466826
Lusiana2312 <lusiana.torres@siemens-energy.com>
2025-09-30 09:57:49 -0500

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -19,7 +19,7 @@ if usuario not in usuarios_autorizados or usuarios_autorizados[usuario] != clave
 st.success("✅ Acceso concedido")
 
 # 🧭 Selector de equipo
-equipo = st.selectbox("Selecciona el tipo de equipo", ["CTG DPS", "CT", "PT"])
+equipo = st.selectbox("Selecciona el tipo de equipo", ["Descargador de sobretensiones", "Transformador de corriente", "Transformador de tensión", "Interruptor"])
 
 # ▶️ Ejecutar solo la función correspondiente
 try:
@@ -27,13 +27,17 @@ try:
         import generador_dps
         generador_dps.mostrar_app()
 
-    elif equipo == "CT":
+    elif equipo == "Transformador de corriente":
         import generador_ct
         generador_ct.mostrar_app()
 
-    elif equipo == "PT":
+    elif equipo == "Transformador de tensión":
         import generador_pt
         generador_pt.mostrar_app()
+        
+    elif equipo == "Interruptor":
+        import generador_cb
+        generador_cb.mostrar_app()
 
 except ModuleNotFoundError as e:
     st.error(f"❌ No se encontró el módulo: {e.name}")
@@ -41,6 +45,7 @@ except AttributeError:
     st.error("⚠️ El módulo existe pero no tiene la función 'mostrar_app()'. Verifica que esté correctamente definida.")
 except Exception as e:
     st.error(f"⚠️ Ocurrió un error inesperado: {e}")
+
 
 
 

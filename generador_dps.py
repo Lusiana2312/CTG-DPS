53ade2ef9e184eabce36087b8cf9b5a036bca72c
Lusiana2312 <lusiana.torres@siemens-energy.com>
2025-10-07 12:06:40 -0500

    Update generador_dps.py

--- generador_dps.py
+++ generador_dps.py
@@ -221,13 +221,13 @@ def mostrar_app():
     
         # 16
         "Tensión residual al impulso de corriente de escalón (10 kA)": ures_escalon,
-    
-        # 17
+
+        #17
         datos["Tensión residual al impulso tipo maniobra (Ures)"] = (
-            f"250 A\n": ures_maniobra_250,
-            f"Tensión residual al impulso tipo maniobra (Ures) - 500 A\n": ures_maniobra_500
-            f"Tensión residual al impulso tipo maniobra (Ures) - 1000 A\n": ures_maniobra_1000
-            f"Tensión residual al impulso tipo maniobra (Ures) - 2000 A": ures_maniobra_2000
+            f"250 A: {ures_maniobra_250}\n"
+            f"500 A: {ures_maniobra_500}\n"
+            f"1000 A: {ures_maniobra_1000}\n"
+            f"2000 A: {ures_maniobra_2000}"
         )
         # 18
         "Tensión residual al impulso tipo rayo (Ures) - 5 kA": ures_rayo_5ka,
@@ -417,6 +417,7 @@ def mostrar_app():
     
     
     
+
 
 
 

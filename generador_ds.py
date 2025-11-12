5d0fd568f510007246c0b5e71591b6892ee95b26
Lusiana2312 <lusiana.torres@siemens-energy.com>
2025-11-12 09:44:23 -0500

    Create generador_ds.py

---
+++ generador_ds.py
@@ -0,0 +1,17 @@
+# empieza codigo
+import streamlit as st
+from openpyxl import Workbook
+from io import BytesIO
+from datetime import datetime
+import pandas as pd
+from openpyxl.drawing.image import Image
+from openpyxl.styles import Font, Alignment, Border, Side, PatternFill
+import textwrap
+
+
+################## CTG DISCONNECTOW SWITCH// SECCIONADOR
+def mostrar_app():
+    st.set_page_config(page_title="Generador CTG - Interruptor de Potencia", layout="wide")
+
+    st.title("📄 Generador de Ficha CTG")
+    st.subheader("Seccionador")
